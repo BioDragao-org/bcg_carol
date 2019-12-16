7f28b2c19025847a5565f4e411cdd92e9bf92205
Emilyn Costa <emilyncosta@gmail.com>
2019-12-16 10:30:09 -0200

    initial commit for carols genomes

---
+++ rdanalyzer_carol_genomes.sh
@@ -0,0 +1,63 @@
+echo "START 10BCG_S20_R2_001.p.fastq"
+python2.7 rdanalyzer.py -o 10BCG 10BCG_S20_R2_001.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 11BCG_S32_R1_001.p.fastq"
+python2.7 rdanalyzer.py -o 11BCG 11BCG_S32_R1_001.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 12BCG_S44_R2_001.p.fastq"
+python2.7 rdanalyzer.py -o 12BCG 12BCG_S44_R2_001.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 17BCG_S9_R1_001.p.fastq"
+python2.7 rdanalyzer.py -o 17BCG 17BCG_S9_R1_001.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 30BCG_S9_R2.p.fastq"
+python2.7 rdanalyzer.py -o 30BCG 30BCG_S9_R2.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 34BCG_S20_R1.p.fastq"
+python2.7 rdanalyzer.py -o 34BCG 34BCG_S20_R1.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 37BCG_S40_R1.p.fastq"
+python2.7 rdanalyzer.py -o 37BCG 37BCG_S40_R1.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 58BCG_S4_R1.p.fastq"
+python2.7 rdanalyzer.py -o 58BCG 58BCG_S4_R1.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 59BCG_S24_R1.p.fastq"
+python2.7 rdanalyzer.py -o 59BCG 59BCG_S24_R1.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 60BCG_S11_R1.p.fastq"
+python2.7 rdanalyzer.py -o 60BCG 60BCG_S11_R1.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 61BCG_S1_R1.p.fastq"
+python2.7 rdanalyzer.py -o 61BCG 61BCG_S1_R1.p.fastq
+echo "DONE"
+echo "====================="
+
+echo "START 8BCG_S91_R1_001.p.fastq"
+python2.7 rdanalyzer.py -o 8BCG 8BCG_S91_R1_001.p.fastq
+echo "DONE"
+echo "====================="
+
+
+
+
