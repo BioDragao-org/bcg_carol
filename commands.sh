0f21da7c2fa7da16841d68a6c886114bb072ac47
Emilyn Costa <emilyncosta@gmail.com>
2019-12-16 19:13:40 -0200

    added the command for spotyping

---
+++ commands.sh
@@ -0,0 +1,33 @@
+## NOTE: Run the analysis only for one genome at a time - inside the virtualbox
+
+#python2.7 SpoTyping.py ./8BCG_S91_R1_001.p.fastq  -o 8BCG.txt
+
+#python2.7 SpoTyping.py ./10BCG_S20_R2_001.p.fastq -o 10BCG.txt
+
+#python2.7 SpoTyping.py ./11BCG_S32_R1_001.p.fastq -o 11BCG.txt
+
+#python2.7 SpoTyping.py ./12BCG_S44_R2_001.p.fastq -o 12BCG.txt
+
+#python2.7 SpoTyping.py ./17BCG_S9_R1_001.p.fastq -o 17BCG.txt
+
+#python2.7 SpoTyping.py ./30BCG_S9_R2.p.fastq -o 30BCG.txt
+
+#python2.7 SpoTyping.py ./34BCG_S20_R1.p.fastq -o 34BCG.txt
+
+#python2.7 SpoTyping.py ./37BCG_S40_R1.p.fastq -o 37BCG.txt
+
+#python2.7 SpoTyping.py ./58BCG_S4_R1.p.fastq -o 58BCG.txt
+
+#python2.7 SpoTyping.py ./59BCG_S24_R1.p.fastq -o 59BCG.txt
+
+#python2.7 SpoTyping.py ./61BCG_S1_R1.p.fastq -o 61BCG.txt
+
+#python2.7 SpoTyping.py ./8BCG_S91_R1_001.p.fastq -o 8BCG.txt
+
+
+
+# PROBLEMATIC GENOME
+###python2.7 SpoTyping.py ./60BCG_S11_R1.p.fastq -o 60BCG.txt
+
+
+
