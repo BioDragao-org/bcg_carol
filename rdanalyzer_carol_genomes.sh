4130af8e98b929b332f0003339ec44b62710256e
Abhinav Sharma <abhi18av@gmail.com>
2019-12-18 17:06:00 +0000

    add list of downloaded carol's genomes

--- rdanalyzer_carol_genomes.sh
+++ rdanalyzer_carol_genomes.sh
@@ -61,3 +61,41 @@ echo "====================="
 
 
 
+10BCG_S20_R1_001.p.fastq.gz
+10BCG_S20_R2_001.p.fastq.gz
+11BCG_S32_R1_001.p.fastq.gz
+11BCG_S32_R2_001.p.fastq.gz
+12BCG_S44_R1_001.p.fastq.gz
+12BCG_S44_R2_001.p.fastq.gz
+17BCG_S9_R1_001.p.fastq.gz
+17BCG_S9_R2_001.p.fastq.gz
+1BCG_S7_R1_001.p.fastq.gz
+1BCG_S7_R2_001.p.fastq.gz
+2BCG_S19_R1_001.p.fastq.gz
+2BCG_S19_R2_001.p.fastq.gz
+3BCG_S31_R1_001.p.fastq.gz
+3BCG_S31_R2_001.p.fastq.gz
+58BCG_S4_R1.p.fastq.gz
+58BCG_S4_R2.p.fastq.gz
+59BCG_S24_R1.p.fastq.gz
+59BCG_S24_R2.p.fastq.gz
+5BCG_S55_R1_001.p.fastq.gz
+5BCG_S55_R2_001.p.fastq.gz
+60BCG_S11_R1.p.fastq.gz
+60BCG_S11_R2.p.fastq.gz
+61BCG_S1_R1.p.fastq.gz
+61BCG_S1_R2.p.fastq.gz
+6BCG_S67_R1_001.p.fastq.gz
+6BCG_S67_R2_001.p.fastq.gz
+7BCG_S79_R1_001.p.fastq.gz
+7BCG_S79_R2_001.p.fastq.gz
+8BCG_S91_R1_001.p.fastq.gz
+8BCG_S91_R2_001.p.fastq.gz
+9BCG_S8_R1_001.p.fastq.gz
+9BCG_S8_R2_001.p.fastq.gz
+ABCG_S10_R1_001.p.fastq.gz
+ABCG_S10_R2_001.p.fastq.gz
+BBCG_S22_R1_001.p.fastq.gz
+BBCG_S22_R2_001.p.fastq.gz
+CBCG_S34_R1_001.p.fastq.gz
+CBCG_S34_R2_001.p.fastq.gz
