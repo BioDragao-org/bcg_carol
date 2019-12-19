082f891680197b7560d292ea61656e97b23c588c
Emilyn Costa <emilyncosta@gmail.com>
2019-12-19 10:42:55 +0000

    routine commit

--- run_spotyping.sh
+++ run_spotyping.sh
@@ -36,101 +36,150 @@
 
 
 
-10BCG_S20_R1_001.p.fastq.gz
-10BCG_S20_R2_001.p.fastq.gz
-11BCG_S32_R1_001.p.fastq.gz
-11BCG_S32_R2_001.p.fastq.gz
-12BCG_S44_R1_001.p.fastq.gz
-12BCG_S44_R2_001.p.fastq.gz
-13BCG_S10_R1.p.fastq.gz
-13BCG_S10_R2.p.fastq.gz
-14BCG_S68_R1_001.p.fastq.gz
-14BCG_S68_R2_001.p.fastq.gz
-16BCG_S92_R1_001.p.fastq.gz
-16BCG_S92_R2_001.p.fastq.gz
-17BCG_S9_R1_001.p.fastq.gz
-17BCG_S9_R2_001.p.fastq.gz
-18BCG_S21_R1_001.p.fastq.gz
-18BCG_S21_R2_001.p.fastq.gz
-19BCG_S33_R1_001.p.fastq.gz
-19BCG_S33_R2_001.p.fastq.gz
-1BCG_S7_R1_001.p.fastq.gz
-1BCG_S7_R2_001.p.fastq.gz
-20BCG_S45_R1_001.p.fastq.gz
-20BCG_S45_R2_001.p.fastq.gz
-21BCG_S57_R1_001.p.fastq.gz
-21BCG_S57_R2_001.p.fastq.gz
-22BCG_S69_R1_001.p.fastq.gz
-22BCG_S69_R2_001.p.fastq.gz
-23BCG_S81_R1_001.p.fastq.gz
-23BCG_S81_R2_001.p.fastq.gz
-24BCG_S93_R1_001.p.fastq.gz
-24BCG_S93_R2_001.p.fastq.gz
-25BCG_S28_R1.p.fastq.gz
-25BCG_S28_R2.p.fastq.gz
-26BCG_S27_R1.p.fastq.gz
-26BCG_S27_R2.p.fastq.gz
-27BCG_S26_R1.p.fastq.gz
-27BCG_S26_R2.p.fastq.gz
-29BCG_S22_R1.p.fastq.gz
-29BCG_S22_R2.p.fastq.gz
-2BCG_S19_R1_001.p.fastq.gz
-2BCG_S19_R2_001.p.fastq.gz
-30BCG_S9_R1.p.fastq.gz
-30BCG_S9_R2.p.fastq.gz
-31BCG_S8_R1.p.fastq.gz
-31BCG_S8_R2.p.fastq.gz
-33BCG_S21_R1.p.fastq.gz
-33BCG_S21_R2.p.fastq.gz
-34BCG_S20_R1.p.fastq.gz
-34BCG_S20_R2.p.fastq.gz
-35BCG_S5_R1.p.fastq.gz
-35BCG_S5_R2.p.fastq.gz
-36BCG_S34_R1.p.fastq.gz
-36BCG_S34_R2.p.fastq.gz
-37BCG_S40_R1.p.fastq.gz
-37BCG_S40_R2.p.fastq.gz
-3BCG_S31_R1_001.p.fastq.gz
-3BCG_S31_R2_001.p.fastq.gz
-43BCG_S7_R1.p.fastq.gz
-43BCG_S7_R2.p.fastq.gz
-49BCG_S6_R1.p.fastq.gz
-49BCG_S6_R2.p.fastq.gz
-50BCG_S14_R1.p.fastq.gz
-50BCG_S14_R2.p.fastq.gz
-51BCG_S13_R1.p.fastq.gz
-51BCG_S13_R2.p.fastq.gz
-52BCG_S25_R1.p.fastq.gz
-52BCG_S25_R2.p.fastq.gz
-53BCG_S12_R1.p.fastq.gz
-53BCG_S12_R2.p.fastq.gz
-56BCG_S3_R1.p.fastq.gz
-56BCG_S3_R2.p.fastq.gz
-57BCG_S2_R1.p.fastq.gz
-57BCG_S2_R2.p.fastq.gz
-58BCG_S4_R1.p.fastq.gz
-58BCG_S4_R2.p.fastq.gz
-59BCG_S24_R1.p.fastq.gz
-59BCG_S24_R2.p.fastq.gz
-5BCG_S55_R1_001.p.fastq.gz
-5BCG_S55_R2_001.p.fastq.gz
-60BCG_S11_R1.p.fastq.gz
-60BCG_S11_R2.p.fastq.gz
-61BCG_S1_R1.p.fastq.gz
-61BCG_S1_R2.p.fastq.gz
-6BCG_S67_R1_001.p.fastq.gz
-6BCG_S67_R2_001.p.fastq.gz
-7BCG_S79_R1_001.p.fastq.gz
-7BCG_S79_R2_001.p.fastq.gz
-8BCG_S91_R1_001.p.fastq.gz
-8BCG_S91_R2_001.p.fastq.gz
-9BCG_S8_R1_001.p.fastq.gz
-9BCG_S8_R2_001.p.fastq.gz
-ABCG_S10_R1_001.p.fastq.gz
-ABCG_S10_R2_001.p.fastq.gz
-BBCG_S22_R1_001.p.fastq.gz
-BBCG_S22_R2_001.p.fastq.gz
-CBCG_S34_R1_001.p.fastq.gz
-CBCG_S34_R2_001.p.fastq.gz
-Moreau_1.p.fastq.gz
-Moreau_2.p.fastq.gz
+python2.7 SpoTyping.py ./10BCG_S20_R1_001.p.fastq -o 10BCG.txt
+#10BCG_S20_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./11BCG_S32_R1_001.p.fastq -o 11BCG.txt
+#11BCG_S32_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./12BCG_S44_R1_001.p.fastq -o 12BCG.txt
+#12BCG_S44_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./13BCG_S10_R1.p.fastq -o 13BCG.txt
+#13BCG_S10_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./14BCG_S68_R1_001.p.fastq -o 14BCG.txt
+#14BCG_S68_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./16BCG_S92_R1_001.p.fastq -o 16BCG.txt
+#16BCG_S92_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./17BCG_S9_R1_001.p.fastq -o 17BCG.txt
+#17BCG_S9_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./18BCG_S21_R1_001.p.fastq -o 18BCG.txt
+#18BCG_S21_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./19BCG_S33_R1_001.p.fastq -o 19BCG.txt
+#19BCG_S33_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./1BCG_S7_R1_001.p.fastq -o 1BCG.txt
+#1BCG_S7_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./20BCG_S45_R1_001.p.fastq -o 20BCG.txt
+#20BCG_S45_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./21BCG_S57_R1_001.p.fastq -o 21BCG.txt
+#21BCG_S57_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./22BCG_S69_R1_001.p.fastq -o 22BCG.txt
+#22BCG_S69_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./23BCG_S81_R1_001.p.fastq -o 23BCG.txt
+#23BCG_S81_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./24BCG_S93_R1_001.p.fastq -o 24BCG.txt
+#24BCG_S93_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./25BCG_S28_R1.p.fastq -o 25BCG.txt
+#25BCG_S28_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./26BCG_S27_R1.p.fastq -o 26BCG.txt
+#26BCG_S27_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./27BCG_S26_R1.p.fastq -o 27BCG.txt
+#27BCG_S26_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./29BCG_S22_R1.p.fastq -o 29BCG.txt
+#29BCG_S22_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./2BCG_S19_R1_001.p.fastq -o 2BCG.txt
+#2BCG_S19_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./30BCG_S9_R1.p.fastq -o 30BCG.txt
+#30BCG_S9_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./31BCG_S8_R1.p.fastq -o 31BCG.txt
+#31BCG_S8_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./33BCG_S21_R1.p.fastq -o 33BCG.txt
+#33BCG_S21_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./34BCG_S20_R1.p.fastq -o 34BCG.txt
+#34BCG_S20_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./35BCG_S5_R1.p.fastq -o 35BCG.txt
+#35BCG_S5_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./36BCG_S34_R1.p.fastq -o 36BCG.txt
+#36BCG_S34_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./37BCG_S40_R1.p.fastq -o 37BCG.txt
+#37BCG_S40_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./3BCG_S31_R1_001.p.fastq -o 3BCG.txt
+#3BCG_S31_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./43BCG_S7_R1.p.fastq -o 43BCG.txt
+#43BCG_S7_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./49BCG_S6_R1.p.fastq -o 49BCG.txt
+#49BCG_S6_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./50BCG_S14_R1.p.fastq -o 50BCG.txt
+#50BCG_S14_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./51BCG_S13_R1.p.fastq -o 51BCG.txt
+#51BCG_S13_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./52BCG_S25_R1.p.fastq -o 52BCG.txt
+#52BCG_S25_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./53BCG_S12_R1.p.fastq -o 53BCG.txt
+#53BCG_S12_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./56BCG_S3_R1.p.fastq -o 56BCG.txt
+#56BCG_S3_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./57BCG_S2_R1.p.fastq -o 57BCG.txt
+#57BCG_S2_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./58BCG_S4_R1.p.fastq -o 58BCG.txt
+#58BCG_S4_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./59BCG_S24_R1.p.fastq -o 59BCG.txt
+#59BCG_S24_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./5BCG_S55_R1_001.p.fastq -o 5BCG.txt
+#5BCG_S55_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./60BCG_S11_R1.p.fastq -o 60BCG.txt
+#60BCG_S11_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./61BCG_S1_R1.p.fastq -o 61BCG.txt
+#61BCG_S1_R2.p.fastq.gz
+
+python2.7 SpoTyping.py ./6BCG_S67_R1_001.p.fastq -o 6BCG.txt
+#6BCG_S67_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./7BCG_S79_R1_001.p.fastq -o 7BCG.txt
+#7BCG_S79_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./8BCG_S91_R1_001.p.fastq -o 8BCG.txt
+#8BCG_S91_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./9BCG_S8_R1_001.p.fastq -o 9BCG.txt
+#9BCG_S8_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./ABCG_S10_R1_001.p.fastq -o ABCG.txt
+#ABCG_S10_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./BBCG_S22_R1_001.p.fastq -o BBCG.txt
+#BBCG_S22_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./CBCG_S34_R1_001.p.fastq -o CBCG.txt
+#CBCG_S34_R2_001.p.fastq.gz
+
+python2.7 SpoTyping.py ./Moreau_1.p.fastq -o Moreau.txt
+#Moreau_2.p.fastq.gz
+
